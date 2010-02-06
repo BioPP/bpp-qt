5fc46fa3284c2f531ffcac70de70ec4459d76b3b
jdutheil <jdutheil@fbc095a7-5795-46e3-bbeb-5b45de2dd435>
2010-02-06 09:24:28 +0000

    Some bugs fixed.

--- Bpp/Qt/QtGraphicDevice.cpp
+++ Bpp/Qt/QtGraphicDevice.cpp
@@ -46,6 +46,7 @@ knowledge of the CeCILL license and that you accept its terms.
 #include <QGraphicsRectItem>
 #include <QGraphicsTextItem>
 #include <QTextDocument>
+#include <QFontInfo>
 
 //From the STL:
 #include <iostream>
@@ -54,7 +55,7 @@ using namespace std;
 using namespace bpp;
 
 QtGraphicDevice::QtGraphicDevice() :
-  scene_(), supportedLineTypes_(), currentPen_(), currentBrush_(), currentFont_()
+  scene_(), supportedLineTypes_(), currentPen_(), currentBrush_(Qt::SolidPattern), currentFont_()
 {
   supportedLineTypes_[GraphicDevice::LINE_SOLID] = Qt::SolidLine;
   supportedLineTypes_[GraphicDevice::LINE_DASHED] = Qt::DashLine;
@@ -112,15 +113,14 @@ void QtGraphicDevice::setCurrentLineType(short type) throw (Exception)
 void QtGraphicDevice::drawLine(double x1, double y1, double x2, double y2)
 {
   QGraphicsLineItem* item = scene_.addLine(xpos(x1), ypos(y1), xpos(x2), ypos(y2), currentPen_);
-  item->setZValue(qreal(getCurrentLayer()));
+  item->setZValue(-qreal(getCurrentLayer()));
 }
 
 
 void QtGraphicDevice::drawRect(double x, double y, double width, double height, short fill)
 {
-  QGraphicsRectItem* item = scene_.addRect(xpos(x), ypos(y), width * getXUnit(), height * getYUnit(), currentPen_);
-  item->setZValue(qreal(getCurrentLayer()));
-  //painter_.fillRect(xpos(x), ypos(y), width * getXUnit(), height * getYUnit(), QBrush(QtTools::toQt(getCurrentBackgroundColor()), Qt::SolidPattern));
+  QGraphicsRectItem* item = scene_.addRect(xpos(x), ypos(y), width * getXUnit(), height * getYUnit(), currentPen_, currentBrush_);
+  item->setZValue(-qreal(getCurrentLayer()));
 }
 
 void QtGraphicDevice::drawCircle(double x, double y, double radius, short fill)
@@ -134,6 +134,8 @@ void QtGraphicDevice::drawText(double x, double y, const std::string& text, shor
   QString qtext = text.c_str();
   QGraphicsTextItem* item = scene_.addText(qtext, currentFont_);
   QSizeF fsize = item->document()->size();
+  QFontInfo fi(currentFont_);
+  double mar = (fsize.rheight() - fi.pointSize()) / 2;
   if (hpos ==  TEXT_HORIZONTAL_LEFT)
     xset = 0;
   else if (hpos ==  TEXT_HORIZONTAL_RIGHT)
@@ -143,14 +145,15 @@ void QtGraphicDevice::drawText(double x, double y, const std::string& text, shor
   else throw UnvalidFlagException("QtGraphicDevice::drawText(). Unvalid horizontal alignment option.");
 
   if (vpos ==  TEXT_VERTICAL_TOP)
-    yset = fsize.rheight();
+    yset = mar;
   else if (vpos ==  TEXT_VERTICAL_BOTTOM)
-    yset = 0;
+    yset = fi.pixelSize() + mar;
   else if (vpos ==  TEXT_VERTICAL_CENTER)
-    yset = fsize.rheight() / 2;
+    yset = fi.pixelSize() / 2 + mar;
   else throw UnvalidFlagException("QtGraphicDevice::drawText(). Unvalid vertical alignment option.");
 
-  item->setPos(xpos(x) + xset, ypos(y) - yset);
-  item->setZValue(qreal(getCurrentLayer()));
+  item->setPos(xpos(x) + xset, ypos(y) + 2 - yset);
+  item->setDefaultTextColor(currentPen_.color());
+  item->setZValue(-qreal(getCurrentLayer()));
 }
 
