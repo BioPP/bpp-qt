33345c59a9da0d84f2e8e66f50246ef20a622c80
Julien Dutheil <jdutheil@biopp.univ-montp2.fr>
2011-04-28 11:08:12 +0200

    Compatibility update.

--- src/Bpp/Qt/Tree/TreeCanvasControlers.cpp
+++ src/Bpp/Qt/Tree/TreeCanvasControlers.cpp
@@ -48,7 +48,7 @@ using namespace bpp;
 void NodeClickableAreasTreeDrawingListener::afterDrawNode(const DrawNodeEvent& event)
 {
   const TreeDrawing* td = event.getTreeDrawing();
-  double r = td->getDisplaySettings()->pointArea;
+  double r = td->getDisplaySettings().pointArea;
   GraphicDevice* gd = event.getGraphicDevice();
   Cursor cursor     = event.getCursor();
   gd->drawRect(cursor.getX() - r, cursor.getY() - r, 2 * r, 2 * r);
