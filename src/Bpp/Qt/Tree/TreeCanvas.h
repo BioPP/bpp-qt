2342f7aa9a09d238d4aa610f0984f98751beec37
Julien Y.  Dutheil <dutheil@evolbio.mpg.de>
2023-05-09 13:55:43 +0200

    Upgrade to Bio++ V3.00.

--- src/Bpp/Qt/Tree/TreeCanvas.h
+++ src/Bpp/Qt/Tree/TreeCanvas.h
@@ -49,7 +49,7 @@ knowledge of the CeCILL license and that you accept its terms.
 #include <string>
 #include <algorithm>
 
-//From PhylLib:
+//From bpp-phyl:
 #include <Bpp/Phyl/Tree/Tree.h>
 #include <Bpp/Phyl/Graphics/TreeDrawing.h>
 #include <Bpp/Phyl/Graphics/AbstractTreeDrawing.h>
